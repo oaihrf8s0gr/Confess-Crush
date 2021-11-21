cc9fee9bbb0a13b79d2cee74f28f873203657cd5
oaihrf8s0gr <94769459+oaihrf8s0gr@users.noreply.github.com>
2021-11-21 09:08:01 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "Gửi cho linh cute phô mai que",
+    introTitle: 'Linh',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
@@ -12,9 +12,9 @@ const CONFIG = {
     btnNo: 'Không nha :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    reply: 'Tại vì thầy quá đẹp trai cute ',
+    mess: 'Thầy biết mà 🥰. Yêu Linh nhiều nhiều 😘😘',
+    messDesc: 'Noel đi chơi với thầy nha.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/messages/t/100032745006340' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
